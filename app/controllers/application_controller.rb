adb4851179c876afba5501a0d427b030e2bef8f9
nov matake <nov@matake.jp>
2011-05-15 02:25:47 +0900

    locale support (without ja.yml)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_locale
+
+  def set_locale
+    lang = unless params[:lang].blank?
+      params[:lang].downcase
+    else
+      request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    end
+    I18n.locale = lang if I18n.available_locales.include?(lang.to_sym)
+  rescue
+    # use default
+  end
 end
